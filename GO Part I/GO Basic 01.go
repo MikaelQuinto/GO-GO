d8fd0b3b0cb9676edeee58a00f5c1c1a6f8941c5
MikaelQ <mikaels.quintor@gmail.com>
2025-03-29 11:14:29 -0500

    A D V A N C E

--- GO Part I/GO Basic 01.go
+++ GO Part I/GO Basic 01.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	var name string = "Nombre"
 	age := "20"
+	dni := "72411224"
 	fmt.Println("Hi, I am " + name + "and I have " + age + "years old")
+	fmt.Println("My DNI is " + dni)
 }
